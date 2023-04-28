9546b6732347ee865633213066528d2342389654
JohnWicc69420 <128295851+JohnWicc69420@users.noreply.github.com>
2023-04-28 16:12:39 +0500

    asd

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import "./Navbar.scss"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -14,7 +15,16 @@ const Navbar = () => {
           <div className="img">
             <img src="https://w7.pngwing.com/pngs/60/776/png-transparent-flag-of-the-united-states-comparison-of-american-and-british-english-flag-of-the-united-kingdom-united-states.png" alt="" />
             <KeyboardArrowDownIcon />
+
+            <div className="img">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
           </div>
+
+          <div></div>
+          </div>
+
+          
         
         </div>
         <div class="center">
