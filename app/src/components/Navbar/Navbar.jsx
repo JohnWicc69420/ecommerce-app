08ed2b4697124f2bc7b70ff90242b3fa5471ac35
JohnWicc69420 <128295851+JohnWicc69420@users.noreply.github.com>
2023-04-28 16:08:31 +0500

    asd

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import "./Navbar.scss"
 
 const Navbar = () => {
   return (
@@ -11,6 +12,7 @@ const Navbar = () => {
       <div className="container">
         <div class="left">
           <div className="img">
+            <img src="https://w7.pngwing.com/pngs/60/776/png-transparent-flag-of-the-united-states-comparison-of-american-and-british-english-flag-of-the-united-kingdom-united-states.png" alt="" />
             <KeyboardArrowDownIcon />
           </div>
         
