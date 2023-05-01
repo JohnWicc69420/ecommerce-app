a20948c1563b81b8f731e4f4092fcede1aed8746
JohnWicc69420 <128295851+JohnWicc69420@users.noreply.github.com>
2023-05-01 14:38:03 +0500

    idk man

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -12,13 +12,15 @@ const Navbar = () => {
     <>
       <div className="container">
         <div className="left">
-          <div className="img">
+             <div className="img">
             <img src="https://w7.pngwing.com/pngs/60/776/png-transparent-flag-of-the-united-states-comparison-of-american-and-british-english-flag-of-the-united-kingdom-united-states.png" alt="" />
             <KeyboardArrowDownIcon />
+             </div>
 
             <div className="img">
             <span>USD</span>
             <KeyboardArrowDownIcon />
+            </div>
 
             <div className="product">
               <Link to="/products/1">Men</Link>
@@ -35,19 +37,27 @@ const Navbar = () => {
             <div className="product">
               <Link to="/products/1">Accessories</Link>
             </div>
-
-          </div>
-
- 
-          </div>
-
-          
-        
         </div>
         <div className="center">
          <h1> ZESAY </h1>
         </div>
         <div className="right">
+
+        <div>
+              <Link to="/">Homepage</Link>
+            </div>
+
+            <div>
+              <Link to="/">About</Link>
+            </div>
+
+            <div>
+              <Link to="/">Contact</Link>
+            </div>
+
+            <div>
+              <Link to="/">Stores</Link>
+            </div>
           <SearchIcon />
           <PersonOutlineOutlinedIcon />
           <FavoriteBorderOutlinedIcon />
